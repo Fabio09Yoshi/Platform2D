2db1e77fab078624b1ea34cd592a6cae0ff8a421
FabioYoshikawa <60529033+Fabio09Yoshi@users.noreply.github.com>
2022-04-16 15:25:35 -0300

    Setup Project + gitignore

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator anim;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        anim.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            anim.SetBool("isJumping", true);
+            jump = true;
+        }
+    }
+
+   
+
+    public void OnLanding()
+    {
+        anim.SetBool("isJumping", false);
+    }
+
+    private void FixedUpdate()
+    {
+        //Movement 
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
